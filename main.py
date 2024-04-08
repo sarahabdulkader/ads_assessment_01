554f8361190322212241fac1ab5938f0d7373639
sarahabdulkader <sarah.abdul.kader@univie.ac.at>
2024-04-08 11:30:45 +0200

    my submission

--- main.py
+++ main.py
@@ -2,9 +2,32 @@
 PowerOfTen
 """
 # Provide your solution here
+num = int(input("Enter a max 3 digit number: "))
+
+if num > 999:
+    print("Number has more than 3 digits.")
+elif num < 1:
+    print("Number cannot be negative.")
+else:
+    print(f"{num} = {num // 100} * 100 + {num % 100 // 10} * 10 + {num % 10} * 1")
 
 
 """
 Cash Box
 """
 # Provide your solution here
+
+while True:
+    owed = int(input("To be paid: "))
+    paid = int(input("Received: "))
+    change = paid - owed
+    if change > 0:
+        print(f"Your change is {change}.")
+        break
+    if paid < 0:
+        print("We dont accept negative amounts.")
+        continue
+    if change < 0:
+        print("You do not have enough money to pay.")
+        continue
+print("Thank you for your purchase.")
